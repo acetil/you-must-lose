4490722723f22a0df6d8d9ac05fab8cf0d72d4df
acetil <acetildev@gmail.com>
2021-09-10 23:42:04 +1000

    Added movement code and adjusted size

--- src/characters/Player.cs
+++ src/characters/Player.cs
@@ -2,19 +2,30 @@ using Godot;
 
 namespace youmustlose.characters {
     public class Player : KinematicBody2D {
-        // Declare member variables here. Examples:
-        // private int a = 2;
-        // private string b = "text";
+
+        [Export] public float movementSpeed = 200.0f;
+
+        private Vector2 movementVel = Vector2.Zero;
 
         // Called when the node enters the scene tree for the first time.
-        public override void _Ready() {
-        
+        public override void _Ready () {
+            
         }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+        private void handleInput() {
+            movementVel.x = 0;
+            if (Input.IsActionPressed("movement_left")) {
+                movementVel.x -= movementSpeed;
+            }
+
+            if (Input.IsActionPressed("movement_right")) {
+                movementVel.x += movementSpeed;
+            }
+        }
+
+        public override void _PhysicsProcess(float delta) {
+            handleInput();
+            movementVel = MoveAndSlide(movementVel);
+        }
     }
 }
