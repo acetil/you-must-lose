9abaef63531c8907ecdeec2e8bf850591de6584c
acetil <acetildev@gmail.com>
2021-09-11 00:45:53 +1000

    Added jump

--- src/characters/Player.cs
+++ src/characters/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace youmustlose.characters {
@@ -5,18 +6,26 @@ namespace youmustlose.characters {
 
         [Export] public float relativeMovementSpeed = 20.0f;
 
-        [Export] public Vector2 gravity = new Vector2(0.0f, 10.0f);
+        [Export] public Vector2 gravityRel = new Vector2(0.0f, 10.0f);
+
+        [Export] public Vector2 jumpImpulseNorm = new Vector2(0.0f, 100.0f);
 
         private Vector2 velocity = Vector2.Zero;
+        private Vector2 gravity;
         private Vector2 movementVel = Vector2.Zero;
 
         private const float SPEED_MULT = 1000.0f;
+        private const float GRAVITY_MULT = 10.0f;
 
         private float movementSpeed = 0.0f;
+
+        private bool jumped = false;
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready () {
             movementSpeed = relativeMovementSpeed * SPEED_MULT;
+            gravity = gravityRel * GRAVITY_MULT;
+            jumpImpulseNorm.y *= -1;
         }
 
         private Vector2 handleInput (float delta) {
@@ -30,14 +39,28 @@ namespace youmustlose.characters {
                 movementVel.x += movementSpeed * delta;
             }
 
+            if (Input.IsActionPressed("movement_jump") && !jumped) {
+                deltaVel.y -= velocity.y;
+                deltaVel += jumpImpulseNorm;
+                jumped = true;
+            }
+            
             deltaVel += movementVel;
             return deltaVel;
         }
 
         public override void _PhysicsProcess(float delta) {
             velocity += handleInput(delta);
-            velocity += gravity;
-            velocity = MoveAndSlide(velocity);
+            velocity += gravity * delta;
+            var coll  = MoveAndCollide(velocity);
+
+            if (coll != null) {
+                velocity = velocity.Slide(coll.Normal);
+                if (coll.Normal.x == 0) {
+                    jumped = false;
+                }
+            }
+            
         }
     }
 }
