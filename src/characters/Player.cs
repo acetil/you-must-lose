15ce4313ba8d2222447f5299abbe158341a41b4c
acetil <acetildev@gmail.com>
2021-09-11 00:57:31 +1000

    Added multiple jumps

--- src/characters/Player.cs
+++ src/characters/Player.cs
@@ -10,16 +10,20 @@ namespace youmustlose.characters {
 
         [Export] public Vector2 jumpImpulseNorm = new Vector2(0.0f, 100.0f);
 
+        [Export] public int maxJumps = 2;
+
         private Vector2 velocity = Vector2.Zero;
         private Vector2 gravity;
         private Vector2 movementVel = Vector2.Zero;
 
         private const float SPEED_MULT = 1000.0f;
-        private const float GRAVITY_MULT = 10.0f;
+        private const float GRAVITY_MULT = 100.0f;
 
         private float movementSpeed = 0.0f;
 
         private bool jumped = false;
+
+        private int jumps = 0;
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready () {
@@ -39,12 +43,17 @@ namespace youmustlose.characters {
                 movementVel.x += movementSpeed * delta;
             }
 
-            if (Input.IsActionPressed("movement_jump") && !jumped) {
-                deltaVel.y -= velocity.y;
-                deltaVel += jumpImpulseNorm;
-                jumped = true;
+            if (Input.IsActionPressed("movement_jump")) {
+                if (jumps < maxJumps && !jumped) {
+                    Console.WriteLine("Jumped!");
+                    deltaVel.y -= velocity.y;
+                    deltaVel += jumpImpulseNorm;
+                    jumps++;
+                    jumped = true;
+                }
+            } else {
+                jumped = false;
             }
-            
             deltaVel += movementVel;
             return deltaVel;
         }
@@ -52,15 +61,18 @@ namespace youmustlose.characters {
         public override void _PhysicsProcess(float delta) {
             velocity += handleInput(delta);
             velocity += gravity * delta;
-            var coll  = MoveAndCollide(velocity);
+            var coll  = MoveAndCollide(velocity * delta);
 
             if (coll != null) {
+                Console.WriteLine("Vel: " + velocity);
                 velocity = velocity.Slide(coll.Normal);
+                Console.WriteLine("Vel: " + velocity);
                 if (coll.Normal.x == 0) {
-                    jumped = false;
+                    jumps = 0;
                 }
+
+                velocity = MoveAndSlide(velocity);
             }
-            
         }
     }
 }
